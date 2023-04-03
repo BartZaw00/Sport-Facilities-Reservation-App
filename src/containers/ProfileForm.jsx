e497648f034bd2010916d86a24d060bf0af35520
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-04-03 12:00:22 +0200

    Design of the Profile Modal changed

--- src/containers/ProfileForm.jsx
+++ src/containers/ProfileForm.jsx
@@ -17,16 +17,18 @@ const ProfileForm = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const [isEditMode, setIsEditMode] = useState(false);
+  const [isEditProfileMode, setIsEditProfileMode] = useState(false);
+  const [isEditPasswordMode, setIsEditPasswordMode] = useState(false);
+
   const [success, setSuccess] = useState(false);
   const [successMsg, setSuccessMsg] = useState("");
   const [errMsg, setErrMsg] = useState("");
 
   useEffect(() => {
-    if (isEditMode) {
+    if (isEditProfileMode) {
       usernameRef.current.focus();
     }
-  }, [isEditMode]);
+  }, [isEditProfileMode]);
 
   useEffect(() => {
     setErrMsg("");
@@ -35,27 +37,43 @@ const ProfileForm = () => {
   useEffect(() => {
     setUserId(user.id);
     setUsername(user.username);
-    setName(user.name);
-    setSurname(user.surname);
+    setName(user.name || "");
+    setSurname(user.surname || "");
     setEmail(user.email);
+    //setPhotoUrl()
   }, [user]);
 
   const updateUserDetails = async () => {
+    console.log("DANE");
+    console.log("username:" + username);
+    console.log("username:" + name);
+    console.log("username:" + surname);
+    console.log("username:" + email);
+    console.log("username:" + password);
+    console.log("username:" + photoUrl);
     fetch(`${import.meta.env.VITE_ACCOUNT_URL}/update/${user.id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({username, name, surname, email, password, photoUrl}),
+      body: JSON.stringify({
+        username,
+        name,
+        surname,
+        email,
+        password,
+        photoUrl,
+      }),
     })
       .then(async (response) => {
         console.log(response);
         const data = await response.json();
         console.log(data);
         setSuccess(true);
+        setSuccessMsg("Pomyślnie udało się zaktualizować profil");
         setTimeout(() => {
           setSuccess(false);
-        }, 2000);
+        }, 5000);
       })
       .catch((error) => {
         console.log(error);
@@ -65,8 +83,12 @@ const ProfileForm = () => {
       });
   };
 
-  const handleEditClick = () => {
-    setIsEditMode(true);
+  const handleEditProfileClick = () => {
+    setIsEditProfileMode(true);
+  };
+
+  const handleChangePasswordClick = () => {
+    setIsEditPasswordMode(true);
   };
 
   const handleSaveClick = async () => {
@@ -82,12 +104,13 @@ const ProfileForm = () => {
       setErrMsg("Podaj Hasło.");
       return;
     }
-    setIsEditMode(false);
+    setIsEditProfileMode(false);
     await updateUserDetails();
   };
 
   const handleCancelClick = () => {
-    setIsEditMode(false);
+    if (isEditProfileMode) setIsEditProfileMode(false);
+    if (isEditPasswordMode) setIsEditPasswordMode(false);
     // TODO: Implement cancel changes logic
   };
 
@@ -113,82 +136,132 @@ const ProfileForm = () => {
         {errMsg}
       </p>
       <div className="flex flex-col gap-5 px-10 pt-6 pb-10">
-        <div className="flex justify-center items-center gap-4">
-          <ProfilePicture src="src\assets\orlik.jpg" alt="Profile picture" />
-          <div className="flex flex-col gap-4">
-            <FormButton className="px-4 py-2 bg-my-primary text-my-primary-bg rounded-md hover:bg-my-primary-hover">
-              Zmień zdjęcie
-            </FormButton>
-            <FormButton className="px-4 py-2 bg-my-primary-bg text-my-primary-text border-2 border-gray-300 rounded-md hover:bg-gray-200">
-              Usuń zdjęcie
-            </FormButton>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-wrap gap-4">
-            <FormInput
-              label="Imię"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              type="text"
-              isEditMode={isEditMode}
-            />
-            <FormInput
-              label="Nazwisko"
-              id="surname"
-              value={surname}
-              onChange={(e) => setSurname(e.target.value)}
-              type="text"
-              isEditMode={isEditMode}
-            />
-          </div>
-
-          <div className="flex-1 flex flex-col gap-2">
-            <label htmlFor="username" className="text-my-gray">
-              Nazwa Użytkownika
-            </label>
-            {isEditMode ? (
-              <input
-                type="text"
-                id="username"
-                ref={usernameRef}
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-my-primary"
-              />
-            ) : (
-              <div className="px-4 py-2 border border-gray-300 rounded-md">
-                {username}
+        {isEditPasswordMode || (
+          <div className="flex justify-center items-center gap-4">
+            <ProfilePicture src="src\assets\orlik.jpg" alt="Profile picture" />
+            {isEditProfileMode && (
+              <div className="flex flex-col gap-4">
+                <FormButton className="px-4 py-2 bg-my-primary text-my-primary-bg rounded-md hover:bg-my-primary-hover">
+                  Zmień zdjęcie
+                </FormButton>
+                <FormButton className="px-4 py-2 bg-my-primary-bg text-my-primary-text border-2 border-gray-300 rounded-md hover:bg-gray-200">
+                  Usuń zdjęcie
+                </FormButton>
               </div>
             )}
           </div>
-          <FormInput
-            label="Email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            isEditMode={isEditMode}
-          />
-          <FormInput
-            label="Hasło"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            isEditMode={isEditMode}
-          />
-          {isEditMode ? (
+        )}
+        <div className="flex flex-col gap-4">
+          {isEditPasswordMode ? (
+            <>
+              <div className="flex-1 flex flex-col gap-2">
+                <label htmlFor="password" className="text-my-gray">
+                  Hasło
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="*********"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-my-primary"
+                />
+              </div>
+              <div className="flex-1 flex flex-col gap-2">
+                <label htmlFor="confirm_password" className="text-my-gray">
+                  Potwierdź Hasło
+                </label>
+                <input
+                  type="password"
+                  id="confirm_password"
+                  placeholder="*********"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-my-primary"
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex flex-wrap gap-4">
+                <div className="flex-1 flex flex-col gap-2">
+                  <label htmlFor="name" className="text-my-gray">
+                    Imię
+                  </label>
+                  {isEditProfileMode ? (
+                    <input
+                      type="text"
+                      id="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-my-primary"
+                    />
+                  ) : (
+                    <div className="px-4 py-2 border border-gray-300 rounded-md">
+                      {name}
+                    </div>
+                  )}
+                </div>
+                <div className="flex-1 flex flex-col gap-2">
+                  <label htmlFor="surname" className="text-my-gray">
+                    Nazwisko
+                  </label>
+                  {isEditProfileMode ? (
+                    <input
+                      type="text"
+                      id="surname"
+                      value={surname}
+                      onChange={(e) => setSurname(e.target.value)}
+                      className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-my-primary"
+                    />
+                  ) : (
+                    <div className="px-4 py-2 border border-gray-300 rounded-md">
+                      {surname}
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="flex-1 flex flex-col gap-2">
+                <label htmlFor="username" className="text-my-gray">
+                  Nazwa Użytkownika
+                </label>
+                {isEditProfileMode ? (
+                  <input
+                    type="text"
+                    id="username"
+                    ref={usernameRef}
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-my-primary"
+                  />
+                ) : (
+                  <div className="px-4 py-2 border border-gray-300 rounded-md">
+                    {username}
+                  </div>
+                )}
+              </div>
+              <div className="flex-1 flex flex-col gap-2">
+                <label htmlFor="email" className="text-my-gray">
+                  Email
+                </label>
+                {isEditProfileMode ? (
+                  <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-my-primary"
+                  />
+                ) : (
+                  <div className="px-4 py-2 border border-gray-300 rounded-md">
+                    {email}
+                  </div>
+                )}
+              </div>
+            </>
+          )}
+          {isEditProfileMode || isEditPasswordMode ? (
             <div className="flex flex-col gap-4">
-              <FormInput
-                label="Potwierdź hasło"
-                id="confirm-password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                type="password"
-                isEditMode={isEditMode}
-              />
               <div className="flex gap-4">
                 <FormButton
                   onClick={handleSaveClick}
@@ -205,12 +278,20 @@ const ProfileForm = () => {
               </div>
             </div>
           ) : (
-            <button
-              className="px-4 py-2 bg-my-primary text-white rounded-md hover:bg-my-primary-hover focus:outline-none"
-              onClick={handleEditClick}
-            >
-              Edytuj profil
-            </button>
+            <>
+              <button
+                className="px-4 py-2 bg-my-primary text-white rounded-md hover:bg-my-primary-hover focus:outline-none"
+                onClick={handleEditProfileClick}
+              >
+                Edytuj Profil
+              </button>
+              <button
+                className="px-4 py-2 bg-my-primary text-white rounded-md hover:bg-my-primary-hover focus:outline-none"
+                onClick={handleChangePasswordClick}
+              >
+                Zmień Hasło
+              </button>
+            </>
           )}
         </div>
       </div>
